5b458eb7d8bd00c1a959265ae4bc73c2aad3e88e
cryptonomicon46 <sandman2145@gmail.com>
2022-02-28 23:14:24 -0800

    Ropsten connects

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,6 +1,10 @@
 const Token = artifacts.require("Token");
 
 module.exports = function(_deployer) {
+
+    // require("dotenv").config()
+
+
   // Use deployer to state migration tasks.
   _deployer.deploy(Token,10000);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -19,9 +19,14 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+require("dotenv").config()
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const { projectId, mnemonic, privateKey} = require('./secrets.json');
+console.log(projectId)
+console.log(mnemonic)
+console.log(privateKey)
 
 module.exports = {
   /**
@@ -44,8 +49,16 @@ module.exports = {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    }
+     network_id: 5777,       // Any network (default: none)
+    },
+    kovan: {
+        provider:  ()=> new HDWalletProvider(privateKey, "https://kovan.infura.io/v3/" + projectId),
+        // provider: new HDWalletProvider(process.env.DEPLOYMENT_ACCOUNT_KEY, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+	    network_id: 42,
+	    gas: 5000000,
+		gasPrice: 5000000000, // 5 Gwei
+		skipDryRun: true
+	  },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -57,14 +70,20 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+        // provider: () => 
+        // new HDWalletProvider(
+        //   process.env.MNEMONIC,
+        //   `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        // ),  
+    provider: ()=> new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + projectId),
+  
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
@@ -92,6 +111,14 @@ module.exports = {
       // }
     }
   },
+
+//   kovan: {
+//     provider: new HDWalletProvider(process.env.DEPLOYMENT_ACCOUNT_KEY, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+//     network_id: 42,
+//     gas: 5000000,
+//     gasPrice: 5000000000, // 5 Gwei
+//     skipDryRun: true
+//   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
